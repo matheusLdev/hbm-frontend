8b21c4b6c520a524d242f7b9b81a5567c44571f4
matheusldev <mgdesenvolvedor@gmail.com>
2025-02-14 01:35:15 -0300

    feat: adicionando hook useSocket para gerenciar comunicação em tempo real

---
+++ src/hooks/useSocket.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { socket } from "@/services/socket";
+import { Irregularity } from "@/types/socket";
+
+export function useSocket() {
+  const [heartBeat, setHeartBeat] = useState<number | null>(null);
+  const [alert, setAlert] = useState<string | null>(null);
+  const [history, setHistory] = useState<Irregularity[]>([])
+
+  useEffect(() => {
+    socket.on("receiveHeartbeat", (data: number) => {
+      setHeartBeat(data);
+    });
+
+    socket.on("receiveAlert", (alert: string) => {
+      setAlert(`${alert}`);
+      setTimeout(() => setAlert(null), 8000);
+    });
+
+    socket.on("receiveHistory", (data: Irregularity[]) => {
+      setHistory(data);
+    });
+
+    socket.emit("requestHistory");
+
+    return () => {
+      socket.off("receiveHeartbeat");
+      socket.off("receiveAlert");
+      socket.off("receiveHistory");
+    };
+  }, []);
+
+  return { heartBeat, alert, history };
+}
